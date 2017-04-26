ea274e5a4f84a096721f7befd330c9c235ade6ab
a0123055u <festecole06>
2017-04-27 01:01:40 +0800

    First Commit

---
+++ mysite/javascript/app.js
@@ -0,0 +1,8 @@
+var nameApp = angular.module("testController",[]).controller("loginController",function($scope,$http,$window){
+
+var test = function(t1,t2){
+	cosole.log("Enterted THIRU");
+}
+
+
+}
