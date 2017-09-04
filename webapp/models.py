5f757ac84a6acfc5ece471e26b33b1fe0c48317e
m.tiruvaalluvar Murugesan <m.thiruvalluvar@gmail.com>
2017-09-04 19:57:33 +0800

    added changes to model 	modified:   webapp/models.py 	modified:   webapp/templates/webapp/home.html

--- webapp/models.py
+++ webapp/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class user(models.Model):
@@ -13,3 +14,20 @@ class user(models.Model):
     fin=models.CharField(max_length=9,null=True,blank=False)
     password=models.CharField(max_length=100,null=False,blank=False,default='test')
 
+class ProductElectronics(models.Model):
+    modelName = models.CharField(primary_key=True,max_length=100,null=False,blank=False)
+    price = models.IntegerField(null=False,blank=False)
+    company=models.CharField(max_length=100,null=False,blank=False)
+    iventory = models.IntegerField(null=False,blank=False)
+    isActive = models.BooleanField(default=1,null=False,blank=False)
+    selledCount = models.IntegerField(null=False,blank=False)
+    lastUpdated = models.DateTimeField(default=datetime.now(),null=False,blank=False)
+
+
+class ProductOrders(models.Model):
+    orderId = models.CharField(primary_key=True,max_length=6,null=False,blank=False)
+    modelName = models.ForeignKey(ProductElectronics)
+    user = models.CharField(max_length=100,null=False,blank=False)
+    email = models.CharField(max_length=125,null=False,blank=False)
+    quantity = models.IntegerField(null= False,blank=False)
+    dateOfPurchase = models.DateTimeField(default=datetime.now(),null=False,blank=False)
